40ea1cb8e04108e07b2d1bea9b8f31a918610729
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-12-05 22:59:54 -0400

    Implement real logo assets

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,2 +1,2 @@
-@props(['width' => '90', 'url' => '#'])
-<img src="https://picsum.photos/seed/{{ rand(1, 10000) }}/{{ $width }}" alt="placeholder" class="rounded-xl">
+@props(['width' => '90', 'logo' => '#'])
+<img src="{{ asset($logo) }}" width="{{ $width }}" alt="placeholder" class="rounded-xl" />
